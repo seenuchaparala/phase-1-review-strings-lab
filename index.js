d93d8843698356b6ccd7d9495ae413ceb81a0283
Srinivas <seenuchaparala@gmail.com>
2022-03-14 23:15:18 +1100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+let currentUser = "Srinivas";
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`; 
+
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
